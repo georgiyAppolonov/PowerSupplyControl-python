c808125bfd8086447f50cfc70681bbe37fb8e7c6
georgiyAppolonov <gera1234256@yandex.ru>
2022-03-23 13:26:03 +0300

    Add files via upload

--- blockpitaniya.py
+++ blockpitaniya.py
@@ -2,10 +2,18 @@ import serial
 import sys
 import time
 
-adr=0x0
-data= bytearray(26)
+adr=0x0             #Адрес ЛБП (0-30)
+data=bytearray(26)
+
+#----------Стандартные настройки------------------
+stdCurrent  = 100   #Ограничение тока в mA
+voltageStep = 10    #Ступени регулировки напряжения mV
+maxVoltage  = 10000 #Напряжение окончания замеров
+#-------------------------------------------------
+
+
         
-serialPort = serial.Serial(
+serialPort = serial.Serial( #установка настроек порта. 9600 бод, 8 бит, без чётности, 1 стопбит
     port='COM2',
     baudrate=9600,
     parity=serial.PARITY_NONE,
@@ -13,18 +21,23 @@ serialPort = serial.Serial(
     bytesize=serial.EIGHTBITS)
 
 
+#----------Очистка массива данных-----------------
 def clearData():
     for j in range(0, 26):
         data[j]=0
+        
 
-def checkSumm():
+#----------Нахождение суммы данных----------------
+def checkSumm(): 
     for j in range(0, 25):
         if data[25]+data[j]<256:
             data[25]=data[25]+data[j]
         else:
             data[25]=data[25]+data[j]-256
+
             
-def setRemote(adress): #Установка соединения с ЛБП
+#----------Установка соединения с ЛБП-------------
+def setRemote(adress): 
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -32,9 +45,9 @@ def setRemote(adress): #Установка соединения с ЛБП
     data[3]=1
     checkSumm()
     serialPort.write(data)
-    return True
 
-def closeRemote(adress): #закрытие соединения с ЛБП
+#----------Закрыть соединения с ЛБП---------------
+def closeRemote(adress): 
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -43,9 +56,9 @@ def closeRemote(adress): #закрытие соединения с ЛБП
     checkSumm()
     serialPort.write(data)
     serialPort.close()
-    return True
 
-def onVoltage(adress): #Подать напругу!
+#----------Подать напругу!------------------------
+def onVoltage(adress): 
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -53,8 +66,9 @@ def onVoltage(adress): #Подать напругу!
     data[3]=1
     checkSumm()
     serialPort.write(data)
-    
-def offVoltage(adress): #Снять напругу!
+
+#----------Снять напругу!-------------------------
+def offVoltage(adress): 
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -63,8 +77,9 @@ def offVoltage(adress): #Снять напругу!
     checkSumm()
     serialPort.write(data)
 
-def setVoltage(adress, Voltage): #становка напряжения в мВ
-    bytesVoltage = list((Voltage >> i) & 0xFF for i in range(0,32,8))
+#----------Установка напряжения в мВ--------------
+def setVoltage(adress, Voltage): 
+    bytesVoltage = list((Voltage >> i) & 0xFF for i in range(0,32,8)) #разложение большого числа на несколько байт
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -76,8 +91,9 @@ def setVoltage(adress, Voltage): #становка напряжения в мВ
     checkSumm()
     serialPort.write(data)
 
-def setCurrent(adress, Current): #становка тока в мА
-    bytesCurrent = list((Current >> i) & 0xFF for i in range(0,16,8))
+#----------Установка тока в мА--------------------
+def setCurrent(adress, Current): 
+    bytesCurrent = list((Current >> i) & 0xFF for i in range(0,16,8)) #разложение большого числа на несколько байт
     clearData()
     data[0]=0xAA
     data[1]=adress
@@ -87,6 +103,7 @@ def setCurrent(adress, Current): #становка тока в мА
     checkSumm()
     serialPort.write(data)
 
+#----------Собстно сам код------------------------
 setRemote(adr)
 print("connected")
 setCurrent(adr, 100)
